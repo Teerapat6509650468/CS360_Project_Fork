b9915fab2fb11134fc5256b9e204cc690894779b
Kwandao6509650245 <145100087+Kwandao6509650245@users.noreply.github.com>
2024-09-21 12:40:14 +0700

    Update http.js

--- src/http.js
+++ src/http.js
@@ -1,17 +1,21 @@
+
 import axios from 'axios';
 
-
-
-const getIP = async () =>{
+// Function to get the public IP
+const getIP = async () => {
     const ip = (await axios.get('https://api.ipify.org?format=json')).data.ip;
-    return ip
-}
+    return ip;
+};
+
+// Function to create Axios instance after fetching the IP
 const createAxiosInstance = async () => {
     const ip = await getIP();
+    return axios.create({
+        baseURL: `http://${ip}:1337/`,  // Use the fetched IP here
+        headers: {
+            "Content-type": "application/json"
+        }
+    });
+};
 
-export default axios.create({
-    baseURL: `http://${ip}:1337/`,
-    headers: {
-        "Content-type": "application/json"
-    }
-});
+export default createAxiosInstance;
