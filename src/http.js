2e74194df07230c75ca37aff9ddfcfe6f409fe07
Kwandao6509650245 <145100087+Kwandao6509650245@users.noreply.github.com>
2024-09-21 11:54:48 +0700

    Update http.js
    replace static localhost to be any public IP

--- src/http.js
+++ src/http.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+const ip = (await axios.get('https://api.ipify.org?format=json')).data.ip;
 export default axios.create({
-    baseURL: "http://localhost:1337/",
+    baseURL: `http://${ip}:1337/`,
     headers: {
         "Content-type": "application/json"
     }
