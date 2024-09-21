4a213c361d36fcd0b589bd21bb2ea83917247a27
Panapol Junjerm <panapol.jun@dome.tu.ac.th>
2024-09-21 14:18:07 +0700

    add dynamic code IP address in http.js

--- src/http.js
+++ src/http.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+var url="34.239.126.140";
+
 export default axios.create({
-    baseURL: "http://localhost:1337/",
+
+    baseURL: "http://" + url +":1337/",
     headers: {
         "Content-type": "application/json"
     }
