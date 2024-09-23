ccacec10706bdb37b80e1e42200077e286b8f394
Panapol Junjerm <panapol.jun@dome.tu.ac.th>
2024-09-23 22:43:53 +0700

    update deploy script without git repo

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,14 @@ REPO_URL="https://github.com/Kwandao6509650245/CS360_Project.git"
 URL="localhost"
 
 echo "Update the System."
-sudo yum update -y
+sudo apt update -y
 
 # Function to check for Git installation
 installGit() {
   echo "Checking Git..."
   if ! command -v git &> /dev/null; then
     echo "Git not found."
-    sudo yum install git
+    sudo apt install git
   else
     echo "Git is already installed."
   fi
@@ -22,6 +22,9 @@ installNvm() {
   if ! command -v nvm &> /dev/null; then  # Check if NVM is installed first
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
     source ~/.bashrc  # Source the profile to load NVM
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
   else
     echo "NVM is already installed."
   fi
@@ -34,10 +37,22 @@ installNode16() {
   echo "Node.js version 16 installed and activated."
 }
 
+installYarn() {
+  if ! command -v yarn &> /dev/null; then  # Check if NVM is installed first
+    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+    sudo apt update
+    sudo apt install --no-install-recommends yarn
+  else
+    echo "NVM is already installed."
+  fi
+
+}
+
 installPM2() {
   if ! command -v pm2 &> /dev/null; then
     echo "installing pm2"
-    sudo npm install pm2 -g
+    npm install pm2 -g
   else
     echo "pm2 is already installed."
   fi
@@ -74,6 +89,7 @@ yarn start
 }
 
 configPM2() {
+    local current_dir=$(pwd)
     echo "Configuring ecosystem.config.js file..."
     pm2 init
 
@@ -83,7 +99,7 @@ configPM2() {
 	  apps: [
 	    {
 	      name: 'pet-adoption-backend',
-	      cwd: '/home/ubuntu/CS360_Project/backend',
+	      cwd: '$current_dir/backend',
 	      script: 'npm',
 	      args: 'start',
       	env: {
@@ -118,11 +134,8 @@ echo "checking package.."
 installGit
 installNvm
 installNode16
+installYarn
 installPM2
-cd ~
 ProjectSetup
 
 echo "Script completed!"
-
-
-
