98f01692ba6296cc90d3d8bac567adf65ff00b0a
Teerapat6509650468 <teerapat.kir@dome.tu.ac.th>
2024-10-30 04:31:30 +0700

    add jest.config and package.json
    seperate jest config from package.json

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    testEnvironment: 'jsdom',  // Setting the environment to jsdom for React tests
+    transform: {
+      '^.+\\.(js|jsx)$': 'babel-jest',  // Use babel-jest to transform JavaScript and JSX files
+    },
+    moduleNameMapper: {
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',  // Mock CSS and style imports
+    },
+  };
+  
