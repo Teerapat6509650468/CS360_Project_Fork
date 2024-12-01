d11c92e61fbd4fd3fec5e0b245acbf18e336469e
Punnapa6509650559 <punnapa.tri@dome.tu.ac.th>
2024-12-02 02:05:34 +0700

    Fix user-data.sh script to install and run Docker container

--- user-data.sh
+++ user-data.sh
@@ -7,5 +7,14 @@ sudo apt-get install -y docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
 
+# ตรวจสอบว่า Docker ทำงานได้หรือไม่
+sudo systemctl status docker || exit 1
+
+# Pull Docker image 
+docker pull myapp:latest
+
 # Run the application in a Docker container
-docker run -d -p 80:80 <your-docker-image>
+docker run -d -p 80:80 myapp:latest
+
+# ตรวจสอบว่า container ทำงานหรือไม่
+docker ps
