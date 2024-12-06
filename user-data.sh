09010ba2df5e012a2bdca2a747268497089e7942
Teerapat6509650468 <144798820+Teerapat6509650468@users.noreply.github.com>
2024-12-06 20:06:09 +0700

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -28,17 +28,15 @@ sudo usermod -aG docker ubuntu
 TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 IPV4=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
 
-# Create the .env file with secrets and the public IPv4
-cat <<EOT > /home/ubuntu/.env
-HOST=0.0.0.0
-PORT=1337
-PUBLIC_IPV4=$PUBLIC_IPV4
-APP_KEYS={{APP_KEYS}}
-API_TOKEN_SALT={{API_TOKEN_SALT}}
-ADMIN_JWT_SECRET={{ADMIN_JWT_SECRET}}
-TRANSFER_TOKEN_SALT={{TRANSFER_TOKEN_SALT}}
-JWT_SECRET={{JWT_SECRET}}
-EOT
+# Update the .env file with environment variables (GitHub secrets replaced here)
+echo "HOST=0.0.0.0" >> /home/ubuntu/.env
+echo "PORT=1337" >> /home/ubuntu/.env
+echo "APP_KEYS=PLACEHOLDER_APP_KEYS" >> /home/ubuntu/.env
+echo "API_TOKEN_SALT=PLACEHOLDER_API_TOKEN_SALT" >> /home/ubuntu/.env
+echo "ADMIN_JWT_SECRET=PLACEHOLDER_ADMIN_JWT_SECRET" >> /home/ubuntu/.env
+echo "TRANSFER_TOKEN_SALT=PLACEHOLDER_TRANSFER_TOKEN_SALT" >> /home/ubuntu/.env
+echo "JWT_SECRET=PLACEHOLDER_JWT_SECRET" >> /home/ubuntu/.env
+echo "PUBLIC_IPV4=${IPV4}" >> /home/ubuntu/.env
 
 # Confirm that the .env file has been updated
 echo "Updated .env with EC2 IPv4: $IPV4"
