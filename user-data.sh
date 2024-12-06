02b5910ef823948ec382ab0c70b67cf1b5291bee
Kwandao6509650245 <145100087+Kwandao6509650245@users.noreply.github.com>
2024-12-06 12:37:44 +0700

    make sure that use the same images

--- user-data.sh
+++ user-data.sh
@@ -10,8 +10,8 @@ sudo systemctl enable docker
 sudo usermod -aG docker ubuntu
 newgrp docker
 
-sudo docker pull ${{ secrets.DOCKER_USERNAME }}/pet_adopt_frontend
-sudo docker run -d -p 3000:3000 --name cs360_frontend_container meawrage/pet_adopt_frontend:latest
+sudo docker pull ${{ secrets.DOCKER_USERNAME }}/petadopt_frontend
+sudo docker run -d -p 3000:3000 --name cs360_frontend_container ${{ secrets.DOCKER_USERNAME }}/petadopt_frontend:latest
 
-sudo docker pull ${{ secrets.DOCKER_USERNAME }}/pet_adopt_backend
-sudo docker run -d -p 1337:1337 --name cs360_backend_container meawrage/pet_adopt_backend:latest
+sudo docker pull ${{ secrets.DOCKER_USERNAME }}/petadopt_backend
+sudo docker run -d -p 1337:1337 --name cs360_backend_container ${{ secrets.DOCKER_USERNAME }}/petadopt_backend:latest
