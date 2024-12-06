a9a83a1da37b12dea479a1815f87a96e01449fc3
Kwandao6509650245 <145100087+Kwandao6509650245@users.noreply.github.com>
2024-12-06 13:16:50 +0700

    add env for backend

--- user-data.sh
+++ user-data.sh
@@ -14,4 +14,10 @@ sudo docker pull ${{ secrets.DOCKER_USERNAME }}/petadopt_frontend
 sudo docker run -d -p 3000:3000 --name cs360_frontend_container ${{ secrets.DOCKER_USERNAME }}/petadopt_frontend:latest
 
 sudo docker pull ${{ secrets.DOCKER_USERNAME }}/petadopt_backend
-sudo docker run -d -p 1337:1337 --name cs360_backend_container ${{ secrets.DOCKER_USERNAME }}/petadopt_backend:latest
+sudo docker run -d -p 1337:1337 --name cs360_backend_container \
+          -e APP_KEYS=${{ secrets.APP_KEYS }} \
+          -e API_TOKEN_SALT=${{ secrets.API_TOKEN_SALT }} \
+          -e ADMIN_JWT_SECRET=${{ secrets.ADMIN_JWT_SECRET }} \
+          -e TRANSFER_TOKEN_SALT=${{ secrets.TRANSFER_TOKEN_SALT }} \
+          -e JWT_SECRET=${{ secrets.JWT_SECRET }} \
+          ${{ secrets.DOCKER_USERNAME }}/petadopt_backend:latest
